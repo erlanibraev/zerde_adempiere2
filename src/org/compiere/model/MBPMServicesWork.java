bd9b3090b1e7bf8df3c66a1e7af7ee4652b6a8ce
V.Sokolov <V.Sokolov@localhost>
2013-04-15 18:13:00 +0600

    ???????? ?????? ??????????? ?? ??????/???????

---
+++ src/org/compiere/model/MBPMServicesWork.java
@@ -0,0 +1,179 @@
+/**
+ * 
+ */
+package org.compiere.model;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import org.compiere.util.DB;
+
+/**
+ * Class: model for directory services / works
+ * 
+ * @author V.Sokolov
+ *
+ */
+public class MBPMServicesWork extends X_BPM_ServicesWork {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1837142065443628510L;
+
+	/**
+	 * @param ctx
+	 */
+	public MBPMServicesWork(Properties ctx) {
+		super(ctx);
+	}
+	
+	/**
+	 * @param ctx
+	 * @param bpm_ServicesWork_ID
+	 * @param trxName
+	 */
+	public MBPMServicesWork(Properties ctx, int bpm_ServicesWork_ID,
+			String trxName) {
+		super(ctx, bpm_ServicesWork_ID, trxName);
+	}
+	
+	/**
+	 * @param ctx
+	 * @param rs
+	 * @param trxName
+	 */
+	public MBPMServicesWork(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+	}
+	
+	/* 
+	 * adding tree node automatically check
+	 */
+	@Override
+	protected boolean afterSave(boolean newRecord, boolean success) {
+		
+		int treeId = get_TreeId();
+		
+		if(treeId != 0){
+			MTree tree = new MTree (getCtx(), treeId, get_TrxName());
+			if (tree == null || tree.getAD_Tree_ID() == 0)
+				throw new IllegalArgumentException("No Tree -" + tree);
+			verifyTree(tree);
+		}
+		return true;
+	}
+	
+	private int get_TreeId(){
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		// The name must start with a report hrm_order
+		String sql_ = "select t.ad_tree_id from ad_tree t where t.treeType='SW'";
+		try
+		{
+			pstmt = DB.prepareStatement(sql_, null);
+			rs = pstmt.executeQuery();
+			if (rs.next())
+				return rs.getInt(1);
+		}
+		catch (SQLException e)
+		{
+			log.log(Level.SEVERE, "product", e);
+		}
+		finally
+		{
+			DB.close(rs, pstmt);
+			rs = null; pstmt = null;
+		}	
+
+		return 0;
+	}
+	
+	/**
+	 *  Verify Tree
+	 * 	@param tree tree
+	 */
+	private void verifyTree (MTree_Base tree)
+	{
+		String nodeTableName = tree.getNodeTableName();
+		String sourceTableName = tree.getSourceTableName(true);
+		String sourceTableKey = sourceTableName + "_ID";
+		int AD_Client_ID = tree.getAD_Client_ID();
+		int C_Element_ID = 0;
+		if (MTree.TREETYPE_ElementValue.equals(tree.getTreeType()))
+		{
+			String sql = "SELECT C_Element_ID FROM C_Element "
+				+ "WHERE AD_Tree_ID=" + tree.getAD_Tree_ID();
+			C_Element_ID = DB.getSQLValue(null, sql);
+			if (C_Element_ID <= 0)
+				throw new IllegalStateException("No Account Element found");
+		}
+		
+		//	Delete unused
+		StringBuffer sql = new StringBuffer();
+		sql.append("DELETE ").append(nodeTableName)
+			.append(" WHERE AD_Tree_ID=").append(tree.getAD_Tree_ID())
+			.append(" AND Node_ID NOT IN (SELECT ").append(sourceTableKey)
+			.append(" FROM ").append(sourceTableName)
+			.append(" WHERE AD_Client_ID=").append(AD_Client_ID);
+		if (C_Element_ID > 0)
+			sql.append(" AND C_Element_ID=").append(C_Element_ID);
+		sql.append(")");
+		log.finer(sql.toString());
+		
+		//
+		if (!tree.isAllNodes())
+			return;
+		
+		//	Insert new
+		sql = new StringBuffer();
+		sql.append("SELECT ").append(sourceTableKey)
+			.append(" FROM ").append(sourceTableName)
+			.append(" WHERE AD_Client_ID=").append(AD_Client_ID);
+		if (C_Element_ID > 0)
+			sql.append(" AND C_Element_ID=").append(C_Element_ID);
+		sql.append(" AND ").append(sourceTableKey)
+			.append("  NOT IN (SELECT Node_ID FROM ").append(nodeTableName)
+			.append(" WHERE AD_Tree_ID=").append(tree.getAD_Tree_ID()).append(")");
+		log.finer(sql.toString());
+		
+		//
+		PreparedStatement pstmt = null;
+		try
+		{
+			pstmt = DB.prepareStatement(sql.toString(), get_TrxName());
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next())
+			{
+				int Node_ID = rs.getInt(1);
+				PO node = new MTree_Node(tree, Node_ID);
+				if (!node.save())
+					log.log(Level.SEVERE, "Could not add to " + tree + " Node_ID=" + Node_ID);
+			}
+			rs.close();
+			pstmt.close();
+			pstmt = null;
+		}
+		catch (Exception e)
+		{
+			log.log(Level.SEVERE, "verifyTree", e);
+		}
+		try
+		{
+			if (pstmt != null)
+				pstmt.close();
+			pstmt = null;
+		}
+		catch (Exception e)
+		{
+			pstmt = null;
+		}
+		
+	}	//	verifyTree
+
+}
---
+++ src/org/compiere/model/X_BPM_ServicesWork.java
@@ -0,0 +1,195 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.compiere.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Model for BPM_ServicesWork
+ *  @author Adempiere (generated) 
+ *  @version Release 3.7.0LTS - $Id$ */
+public class X_BPM_ServicesWork extends PO implements I_BPM_ServicesWork, I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20130415L;
+
+    /** the default Constructor */
+    public X_BPM_ServicesWork(Properties ctx)
+    { 
+      super (ctx, null, null);
+    } 
+
+    /** Standard Constructor */
+    public X_BPM_ServicesWork (Properties ctx, int BPM_ServicesWork_ID, String trxName)
+    {
+      super (ctx, BPM_ServicesWork_ID, trxName);
+      /** if (BPM_ServicesWork_ID == 0)
+        {
+			setBPM_ServicesWork_ID (0);
+			setIsSummary (false);
+// N
+			setName (null);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_BPM_ServicesWork (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 3 - Client - Org 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_BPM_ServicesWork[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set BPM_ServicesWork ID.
+		@param BPM_ServicesWork_ID BPM_ServicesWork ID	  */
+	public void setBPM_ServicesWork_ID (int BPM_ServicesWork_ID)
+	{
+		if (BPM_ServicesWork_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_BPM_ServicesWork_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_BPM_ServicesWork_ID, Integer.valueOf(BPM_ServicesWork_ID));
+	}
+
+	/** Get BPM_ServicesWork ID.
+		@return BPM_ServicesWork ID	  */
+	public int getBPM_ServicesWork_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_BPM_ServicesWork_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public org.compiere.model.I_C_Charge getC_Charge() throws RuntimeException
+    {
+		return (org.compiere.model.I_C_Charge)MTable.get(getCtx(), org.compiere.model.I_C_Charge.Table_Name)
+			.getPO(getC_Charge_ID(), get_TrxName());	}
+
+	/** Set Charge.
+		@param C_Charge_ID 
+		Additional document charges
+	  */
+	public void setC_Charge_ID (int C_Charge_ID)
+	{
+		if (C_Charge_ID < 1) 
+			set_Value (COLUMNNAME_C_Charge_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_Charge_ID, Integer.valueOf(C_Charge_ID));
+	}
+
+	/** Get Charge.
+		@return Additional document charges
+	  */
+	public int getC_Charge_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_Charge_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Description.
+		@param Description 
+		Optional short description of the record
+	  */
+	public void setDescription (String Description)
+	{
+		set_Value (COLUMNNAME_Description, Description);
+	}
+
+	/** Get Description.
+		@return Optional short description of the record
+	  */
+	public String getDescription () 
+	{
+		return (String)get_Value(COLUMNNAME_Description);
+	}
+
+	/** Set Summary Level.
+		@param IsSummary 
+		This is a summary entity
+	  */
+	public void setIsSummary (boolean IsSummary)
+	{
+		set_Value (COLUMNNAME_IsSummary, Boolean.valueOf(IsSummary));
+	}
+
+	/** Get Summary Level.
+		@return This is a summary entity
+	  */
+	public boolean isSummary () 
+	{
+		Object oo = get_Value(COLUMNNAME_IsSummary);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Name.
+		@param Name 
+		Alphanumeric identifier of the entity
+	  */
+	public void setName (String Name)
+	{
+		set_Value (COLUMNNAME_Name, Name);
+	}
+
+	/** Get Name.
+		@return Alphanumeric identifier of the entity
+	  */
+	public String getName () 
+	{
+		return (String)get_Value(COLUMNNAME_Name);
+	}
+
+    /** Get Record ID/ColumnName
+        @return ID/ColumnName pair
+      */
+    public KeyNamePair getKeyNamePair() 
+    {
+        return new KeyNamePair(get_ID(), getName());
+    }
+}
