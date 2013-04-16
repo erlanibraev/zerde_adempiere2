09e40bbc7fa4d081915d14881a998cc0a0e1880c
E.Ibraev <E.Ibraev@localhost>
2013-04-16 17:06:52 +0600

    ????? buildCustimization.xml ?? "???????" ???????

---
+++ RunMR.bat
@@ -0,0 +1,5 @@
+@Title Set Information on the revision
+
+@Rem The script creates a file with information about the latest local revision
+
+hg summary > REVISION.INF
---
+++ RunMR.sh
@@ -0,0 +1,3 @@
+# Title: Information on the revision
+# Rem The script creates a file with information about the latest local revision
+hg summary > REVISION.INF
