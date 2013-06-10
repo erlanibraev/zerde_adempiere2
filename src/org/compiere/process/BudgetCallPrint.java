663395cf7f1dd0af5b1738b6f63ad442d648195c
V.Sokolov <V.Sokolov@localhost>
2013-06-10 16:11:13 +0600

    ????? ?? ?????? ??

---
+++ src/org/compiere/process/BudgetCallPrint.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.compiere.process;
+
+import java.util.Properties;
+import org.compiere.util.Env;
+import org.compiere.util.Msg;
+
+import extend.org.compiere.utils.Util;
+
+/**
+ * Add the process in the system and do not forget to attach templates:
+ * 	1. templateFirst
+ * 	2. templateSecond
+ * 	3. templateThree
+ * @author V.Sokolov
+ *
+ */
+public class BudgetCallPrint extends SvrProcess{
+
+	/** Current context		*/
+	private Properties m_ctx;
+	/**	 */
+	private ProcessInfo pi;
+	/**  */
+	String url;
+	StringBuffer param = new StringBuffer("/budget/index.action?");
+	
+	/* 
+	 */
+	@Override
+	protected void prepare() {
+		
+		log.info(" Print Budget Call ");
+		m_ctx = Env.getCtx();
+		pi = getProcessInfo();
+
+		
+		param.append("callID="+getRecord_ID());
+		param.append("&processID="+pi.getAD_Process_ID());
+		
+		url = Util.getHostHttp();
+		
+	}
+				
+
+	/* 
+	 */
+	@Override
+	protected String doIt() throws Exception {
+		
+		if(url.startsWith("http"))
+			Env.startBrowser(url+param);
+		
+		return Msg.translate(m_ctx, "Success");
+	}
+
+}
