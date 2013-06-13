b21e58a56b1e6b5eb834b4e83aab318eb75933d8
E.Ibraev <E.Ibraev@localhost>
2013-06-13 15:32:15 +0600

    ??????????? ?????? ? ???????? ??????

---
+++ src/org/adempiere/apps/graph/BSCLine.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.adempiere.apps.graph;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ * @author Y.Ibrayev
+ *
+ */
+public class BSCLine extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3451396548198450863L;
+
+	public BSCLine() {
+		super();
+		setFocusable(false);
+		setPreferredSize(BSCIndicator.paneldimension);
+	}
+	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		int x1 = getSize().width / 2;
+		int x2 = getSize().width / 2;
+		int y1 = 0;
+		int y2 = getSize().height;
+		
+		g.drawLine(x1, y1, x2, y2);
+		
+		x1 = getSize().width / 2;
+		x2 = getSize().width;
+		y1 = getSize().height / 2;
+		y2 = getSize().height / 2;
+		
+		g.drawLine(x1, y1, x2, y2);
+	}
+}
