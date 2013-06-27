8e434e176019243e6620086d9b7cd59f7063e18b
E Maimanov <E.Maimanov@ERP-IV0000525.erp.kz>
2013-06-27 16:08:38 +0600

    TRM Module

--- src/org/compiere/process/TRM_DepositReport.java
+++ src/org/compiere/process/TRM_DepositReport.java
@@ -2,6 +2,8 @@ package org.compiere.process;
 
 import java.io.File;
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
@@ -21,6 +23,7 @@ import jxl.write.WritableCellFormat;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
 import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
 
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.apps.ADialog;
@@ -37,6 +40,8 @@ public class TRM_DepositReport extends SvrProcess
 
 	private Timestamp BeginPeriod;
 	private Timestamp EndPeriod;
+	
+	private String depositAccount = "1040";
 	
 	@Override
 	protected void prepare() 
@@ -80,6 +85,76 @@ public class TRM_DepositReport extends SvrProcess
 		CreateLines(BeginPeriod, EndPeriod, entry);
 		
 		return null;
+	}
+	
+	private BigDecimal StartPeriodSum(Timestamp date)
+	{
+		StringBuffer sql = new StringBuffer();
+		sql.append("select coalesce(sum(fa.amtacctcr - fa.amtacctdr),0)");
+		sql.append("from fact_acct fa, c_elementvalue ce  ");
+		sql.append("where fa.account_id=ce.c_elementvalue_id ");
+		sql.append("and PostingType='A'	and fa.dateacct < '" + date + "' and ce.value like '" + depositAccount + "%'");
+		
+		BigDecimal totalSum = new BigDecimal(0);
+		
+		try
+		{
+			@SuppressWarnings("deprecation")
+			PreparedStatement pstmt = DB.prepareStatement(sql.toString());
+			ResultSet rs = pstmt.executeQuery();
+			
+			if(rs.next())
+			{
+				if(rs.getBigDecimal(1) != null)
+				{
+					totalSum = totalSum.add(rs.getBigDecimal(1));
+				}
+			}
+			
+			rs.close(); pstmt.close();
+			rs = null; pstmt = null;
+		}
+		catch(Exception ex) {}
+		
+		if(totalSum.intValue() == 0)
+			totalSum = new BigDecimal(1);
+		
+		return totalSum;
+	}
+	
+	private BigDecimal EndPeriodSum(Timestamp date, Timestamp date2)
+	{
+		StringBuffer sql = new StringBuffer();
+		sql.append("select coalesce(sum(fa.amtacctcr - fa.amtacctdr),0)");
+		sql.append("from fact_acct fa, c_elementvalue ce  ");
+		sql.append("where fa.account_id=ce.c_elementvalue_id ");
+		sql.append("and PostingType='A'	and fa.dateacct BETWEEN '" + date + "' AND '" + date2 + "' and ce.value like '" + depositAccount + "%'");
+		
+		BigDecimal totalSum = new BigDecimal(0);
+		
+		try
+		{
+			@SuppressWarnings("deprecation")
+			PreparedStatement pstmt = DB.prepareStatement(sql.toString());
+			ResultSet rs = pstmt.executeQuery();
+			
+			if(rs.next())
+			{
+				if(rs.getBigDecimal(1) != null)
+				{
+					totalSum = totalSum.add(rs.getBigDecimal(1));
+				}
+			}
+			
+			rs.close(); pstmt.close();
+			rs = null; pstmt = null;
+		}
+		catch(Exception ex) {}
+		
+		if(totalSum.intValue() == 0)
+			totalSum = new BigDecimal(1);
+		
+		return totalSum;
 	}
 	
 	@SuppressWarnings("deprecation")
@@ -166,7 +241,7 @@ public class TRM_DepositReport extends SvrProcess
 	    dateFormat.setBorder(Border.BOTTOM, BorderLineStyle.THIN, Colour.BLACK);
 	    dateFormat.setAlignment(Alignment.RIGHT);
 	    dateFormat.setVerticalAlignment(VerticalAlignment.CENTRE);
-	    
+	    	    
 	    Number number;
 		Label label;
 		DateTime time;
@@ -224,6 +299,11 @@ public class TRM_DepositReport extends SvrProcess
 		
 		int counter = 1;
 		
+		BigDecimal startPeriodTotalSum = new BigDecimal(0); //StartPeriodSum(beginperiod);
+		BigDecimal endPeriodTotalSum = new BigDecimal(0);//EndPeriodSum(beginperiod, endperiod);
+		
+		BigDecimal totalPremium = new BigDecimal(0);
+		
 		try
 		{
 			PreparedStatement pstmt = DB.prepareStatement(sql.toString());
@@ -231,32 +311,38 @@ public class TRM_DepositReport extends SvrProcess
 			
 			while(rs.next())
 			{
+				BigDecimal lineStartPeriodSum = rs.getBigDecimal("BeginningBalance") == null ? new BigDecimal(0.0) : rs.getBigDecimal("BeginningBalance");
+				BigDecimal lineEndPeriodSum = rs.getBigDecimal("EndingBalance") == null ? new BigDecimal(0.0) : rs.getBigDecimal("EndingBalance");
+				
+				startPeriodTotalSum = startPeriodTotalSum.add(lineStartPeriodSum);
+				endPeriodTotalSum = endPeriodTotalSum.add(lineEndPeriodSum);
+				
 				number = new Number(col, index, counter++ , nn);
 				sheet.addCell(number);
 				
 				label = new Label(col + 1, index, rs.getString("bankName") == null ? "" : rs.getString("bankName"), valuestyle);
 		    	sheet.addCell(label);
 
-		    	number = new Number(col+2, index, rs.getBigDecimal("BeginningBalance") == null ? 0.0 : rs.getBigDecimal("BeginningBalance").doubleValue(), numcolstyle);
+		    	number = new Number(col+2, index, lineStartPeriodSum.doubleValue(), numcolstyle);
 				sheet.addCell(number);
 				
-				number = new Number(col+3, index, rs.getBigDecimal("EndingBalance") == null ? 0.0 : rs.getBigDecimal("EndingBalance").doubleValue(), numcolstyle);
+				number = new Number(col+3, index, lineEndPeriodSum.doubleValue(), numcolstyle);
 				sheet.addCell(number);
 				
 				number = new Number(col+4, index, rs.getBigDecimal("EstablishSublimit") == null ? 0.0 : rs.getBigDecimal("EstablishSublimit").doubleValue(), numcolstyle);
 				sheet.addCell(number);
 				
-				BigDecimal startBalance = rs.getBigDecimal("BeginningBalance") == null ? new BigDecimal(0) : rs.getBigDecimal("BeginningBalance");
-				BigDecimal endBalance = rs.getBigDecimal("EndingBalance") == null ? new BigDecimal(0) : rs.getBigDecimal("EndingBalance");
-				
-				number = new Number(col+5, index, endBalance.subtract(startBalance).doubleValue() , numcolstyle);
+				number = new Number(col+5, index, lineEndPeriodSum.subtract(lineStartPeriodSum).doubleValue() , numcolstyle);
 				sheet.addCell(number);
 				
 				number = new Number(col+6, index, rs.getBigDecimal("FirstSum") == null ? 0.0 : rs.getBigDecimal("FirstSum").doubleValue(), numcolstyle);
 				sheet.addCell(number);
 				
-				label = new Label(col + 7, index, 0 + "%", valuestyle);
-		    	sheet.addCell(label);
+				number = new Number(col+7, index, lineEndPeriodSum.divide(endPeriodTotalSum, 2, RoundingMode.HALF_UP).doubleValue(), numcolstyle);
+				sheet.addCell(number);
+				
+//				label = new Label(col + 7, index, 0 + "%", valuestyle);
+//		    	sheet.addCell(label);
 				
 				label = new Label(col + 8, index, rs.getBigDecimal("InterestRate") == null ? "" : rs.getBigDecimal("InterestRate").doubleValue() + "%", nn);
 		    	sheet.addCell(label);
@@ -285,9 +371,13 @@ public class TRM_DepositReport extends SvrProcess
 					time = new DateTime(col+11, index, new java.sql.Date(rs.getDate("EndDateExecution").getTime()), dateFormat);
 					sheet.addCell(time);
 				}
+				
+				BigDecimal premium = rs.getBigDecimal("Premium") == null ? new BigDecimal(0) : rs.getBigDecimal("Premium");
         		
-        		number = new Number(col+12, index, rs.getBigDecimal("Premium") == null ? new BigDecimal(0).doubleValue() : rs.getBigDecimal("Premium").doubleValue(), numcolstyle);
+        		number = new Number(col+12, index, premium.doubleValue(), numcolstyle);
 				sheet.addCell(number);
+				
+				totalPremium = totalPremium.add(premium);
 				
 				boolean isPartialWithdrawal = rs.getString(11).equals("Y");
 				
@@ -308,6 +398,8 @@ public class TRM_DepositReport extends SvrProcess
 		    	
 		    	index++;
 			}
+			
+			LoadLine(sheet, startPeriodTotalSum, endPeriodTotalSum, totalPremium, totalrowstyle, totalrowstyle,  col, index);
 			
 			copy.write();
 		    copy.close();
@@ -331,6 +423,64 @@ public class TRM_DepositReport extends SvrProcess
 		catch(Exception ex){}
 	}
 
-	
+	private void LoadLine(WritableSheet sheet, BigDecimal startSum, BigDecimal endSum, BigDecimal premium, WritableCellFormat valuestyle, WritableCellFormat numcolstyle, int col, int index) throws RowsExceededException, WriteException
+	{
+		Number number;
+		Label label;
+		
+		label = new Label(col, index, "", valuestyle);
+    	sheet.addCell(label);
+		
+		label = new Label(col + 1, index, "Итого", valuestyle);
+    	sheet.addCell(label);
+
+    	number = new Number(col+2, index, startSum.doubleValue(), numcolstyle);
+		sheet.addCell(number);
+		
+		number = new Number(col+3, index, endSum.doubleValue(), numcolstyle);
+		sheet.addCell(number);
+		
+		label = new Label(col + 4, index, "", valuestyle);
+    	sheet.addCell(label);
+				
+		number = new Number(col+5, index, endSum.subtract(startSum).doubleValue() , numcolstyle);
+		sheet.addCell(number);
+		
+		label = new Label(col + 6, index, "", valuestyle);
+    	sheet.addCell(label);
+		
+    	label = new Label(col + 7, index, "100%", valuestyle);
+    	sheet.addCell(label);
+		
+    	label = new Label(col + 8, index, "", valuestyle);
+    	sheet.addCell(label);
+    	
+    	label = new Label(col + 9, index, "", valuestyle);
+    	sheet.addCell(label);
+		
+    	label = new Label(col + 10, index, "", valuestyle);
+    	sheet.addCell(label);
+		
+    	label = new Label(col + 11, index, "", valuestyle);
+    	sheet.addCell(label);
+		
+		number = new Number(col+12, index, premium.doubleValue(), numcolstyle);
+		sheet.addCell(number);
+		
+		label = new Label(col + 13, index, "", valuestyle);
+    	sheet.addCell(label);
+    	
+    	label = new Label(col + 14, index, "", valuestyle);
+    	sheet.addCell(label);
+    	
+    	label = new Label(col + 15, index, "", valuestyle);
+    	sheet.addCell(label);
+    	
+    	label = new Label(col + 16, index, "", valuestyle);
+    	sheet.addCell(label);
+		
+    	label = new Label(col + 17, index, "", valuestyle);
+    	sheet.addCell(label);
+	}
 	
 }
