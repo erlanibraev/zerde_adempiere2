b5b69448380a9e0bfb70d6fc0cd8c9bb772e5f9b
V.Sokolov <V.Sokolov@localhost>
2013-08-29 19:07:18 +0600

    ?????????? ?????? ??? ??????? ????????? ??????

--- src/org/compiere/process/BudgetCallPrint.java
+++ src/org/compiere/process/BudgetCallPrint.java
@@ -3,7 +3,11 @@
  */
 package org.compiere.process;
 
+import java.math.BigDecimal;
 import java.util.Properties;
+import java.util.logging.Level;
+import org.adempiere.exceptions.AdempiereException;
+import org.compiere.model.I_BPM_BudgetCall;
 import org.compiere.util.Env;
 import org.compiere.util.Ini;
 import org.compiere.util.Msg;
@@ -20,11 +24,13 @@ import extend.org.compiere.utils.Util;
  */
 public class BudgetCallPrint extends SvrProcess{
 
-	/** Current context		*/
+	/* Current context		*/
 	private Properties m_ctx;
-	/**	 */
+	/* */
 	private ProcessInfo pi;
-	/**  */
+	/* */
+	int callID = 0;
+	/* */
 	String url;
 	StringBuffer param = new StringBuffer("/budget/index.action?");
 	
@@ -38,7 +44,24 @@ public class BudgetCallPrint extends SvrProcess{
 		pi = getProcessInfo();
 
 		
-		param.append("callID="+getRecord_ID());
+		ProcessInfoParameter[] para = getParameter();
+		for (int i = 0; i < para.length; i++)	{
+			String name = para[i].getParameterName();
+			if (name == null); 
+				//
+			else if (name.compareTo(I_BPM_BudgetCall.COLUMNNAME_BPM_BudgetCall_ID) == 0 && para[i].getParameter() != null)
+				callID = new BigDecimal(Integer.parseInt(para[i].getParameter().toString())).intValue();
+			else
+			{
+				log.log(Level.INFO, "Unknown Parameter: " + name);
+			}
+		}
+		
+		if(callID == 0)
+			throw new AdempiereException("Неверный параметр заявки. Обратитесь к Администратору.");
+			
+		pi.setRecord_ID(callID);
+		param.append("callID="+pi.getRecord_ID());
 		param.append("&processID="+pi.getAD_Process_ID());
 		param.append("&lang="+Env.getAD_Language(m_ctx));
 
